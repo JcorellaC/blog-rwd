e64fb90cca28db77ea24a931af19bc41b3f63788
unknown <jimmysaw24@gmail.com>
2015-10-20 12:31:22 -0600

    blog responsive de noticias

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	(function($) {
+
+		$('#header_icon').click(function(e){
+			e.preventDefault();
+			$('#menu').removeClass('with--sidebar');
+			$('#header_icon').toggleClass('with--sidebar');
+			$('#header_icon').removeClass('header_icon');
+		});
+    
+    $('#close').click(function(e){
+      $('#menu').toggleClass('with--sidebar');
+      $('#header_icon').removeClass('with--sidebar');
+      $('#header_icon').toggleClass('header_icon');
+    });
+
+	})(jQuery);
+
+});
